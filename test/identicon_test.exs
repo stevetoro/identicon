b38fd274c6b776c57f871f2fe4bc72b1eb52a338
Steve Toro <steve.toro@gmail.com>
2023-12-30 00:02:43 -0700

    Adds unit test for main functionality and identicon image fixtures.

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -1,2 +1,16 @@
 defmodule IdenticonTest do
+  use ExUnit.Case
+
+  test "Identicon.new should generate images deterministically" do
+    file_names = ["elixir", "phoenix", "josevalim"]
+
+    Enum.each(file_names, fn file_name ->
+      image = File.read!("test/fixtures/#{file_name}.png") |> Base.encode64()
+
+      Enum.each(1..2, fn _ ->
+        identicon = Identicon.new(file_name) |> Base.encode64()
+        assert image == identicon, "expected the generated image to match the test fixture"
+      end)
+    end)
+  end
 end
