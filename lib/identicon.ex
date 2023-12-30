4512cedbfec34594f4be70c2ef909045b70d8f64
Steve Toro <steve.toro@gmail.com>
2023-12-29 22:49:22 -0700

    Draws identicon using egd and calculated pixel map.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,7 @@ defmodule Identicon do
     |> set_identicon_color
     |> build_grid
     |> build_pixel_map
+    |> draw_identicon
   end
 
   defp generate_hash(input) do
@@ -51,5 +52,16 @@ defmodule Identicon do
     bottom_right = {horizontal + 50, vertical + 50}
 
     {top_left, bottom_right}
+  end
+
+  defp draw_identicon(%Identicon{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each(pixel_map, fn {start, stop} ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end)
+
+    :egd.render(image)
   end
 end
